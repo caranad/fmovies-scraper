34757b0654f18f3896a3f4420e82428c6dc0e180
BILL-E_MAGNUS\Christopher <christopher.aranadi@mail.utoronto.ca>
2019-11-18 21:07:48 -0500

    Make everything as a function for now

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -1,43 +1,51 @@
 const cloudscraper = require('cloudscraper');
 const cheerio = require('cheerio');
 
-class MovieScraper {
-    constructor() {
-        this.url = "https://fmovies.to";
-    }
+const URL_PREFIX = "https://fmovies.to";
 
-    constructURL(movie) {
-        return this.url + "/search?keyword=" + movie;
-    }
-
-    scrapeMovieData(name) {
-        const movie_name = name.toLowerCase().split(" ").join("+");
-        const url = this.constructURL(movie_name);
-        
-        return cloudscraper(url).then((response) => {
-            var x = cheerio(".name", response);
-            return x;
-        }).catch((err) => {
-            return err;
-        })
-    }
-
-    getMovieInfo(name) {
-        let urls = [];
-
-        return this.scrapeMovieData(name).then((response) => {
-            for (let i = 0; i < response.length; i++) {
-                urls.push({
-                    name: response[i].children[0].data,
-                    movieURL: this.url + response[i].attribs.href
-                })
-            }
-
-            return urls;
-        }).catch((err) => {
-            return err;
-        })
-    }
+/**
+ * Creates URL to scrape through
+ * @param {*} name 
+ */
+function constructURL(name) {
+    return URL_PREFIX + '/search?keyword=' + name;
 }
 
-module.exports = MovieScraper;
+/**
+ * Scraper for movie data from fmovies.to
+ * @param {*} name 
+ */
+function scrapeMovieData(name) {
+    const movie_name = name.toLowerCase().split(" ").join("+");
+    const url = constructURL(movie_name);
+    
+    return cloudscraper(url).then((response) => {
+        const x = cheerio(".name", response);
+        return x;
+    }).catch((err) => {
+        return err;
+    })
+}
+
+/**
+ * Takes a movie name and gets the data from fmovies.to
+ * @param {*} name 
+ */
+function getMovieInfo(name) {
+    let urls = [];
+
+    return scrapeMovieData(name).then((response) => {
+        for (let i = 0; i < response.length; i++) {
+            urls.push({
+                name: response[i].children[0].data,
+                movieURL: URL_PREFIX + response[i].attribs.href
+            })
+        }
+
+        return urls;
+    }).catch((err) => {
+        return err;
+    })
+}
+
+module.exports = getMovieInfo;
